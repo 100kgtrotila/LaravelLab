1b6abacab015c72683f5fa431fe9e0fcb08de043
100kgtrotila <hirodevi192@gmail.com>
2025-06-15 14:37:53 +0300

    Лабораторна 11. Колекції

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\DiggingDeeperController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -44,4 +45,8 @@ Route::group($groupData, function () {
     Route::resource('posts', PostController::class)
         ->except(['show'])
         ->names('blog.admin.posts');
+});
+Route::group(['prefix' => 'digging_deeper'], function () {
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+        ->name('digging_deeper.collections');
 });
